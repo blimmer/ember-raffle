81285fcf0dd65a3a23899469ae4c4316d1f91d2d
Ben Limmer <ben@benlimmer.com>
2017-04-30 19:52:13 -0600

    confirm participants

---
+++ tests/acceptance/raffles/raffle/setup/confirm-participants-test.js
@@ -0,0 +1,42 @@
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { expect } from 'chai';
+import startApp from 'raffle/tests/helpers/start-app';
+import destroyApp from 'raffle/tests/helpers/destroy-app';
+import { visit, find } from 'ember-native-dom-helpers';
+import { make } from 'ember-data-factory-guy';
+import testSelector from 'ember-test-selectors';
+
+describe('Acceptance | raffles/raffle/setup/confirm participants', function() {
+  let application, raffle;
+
+  beforeEach(function() {
+    application = startApp();
+    raffle = make('raffle', 'withParticipants');
+  });
+
+  afterEach(function() {
+    destroyApp(application);
+  });
+
+  it('can visit the route', async function() {
+    await visit(`/raffles/${raffle.id}/setup/confirm-participants`);
+    expect(currentURL()).to.equal(`/raffles/${raffle.id}/setup/confirm-participants`);
+  });
+
+  it('renders the raffle-setup-confirm-participants component', async function() {
+    await visit(`/raffles/${raffle.id}/setup/confirm-participants`);
+    expect(find(testSelector('component', 'raffle-setup-confirm-participants'))).to.be.ok;
+  });
+
+  it('goes back to the add-participants route when clicking the edit button', async function() {
+    await visit(`/raffles/${raffle.id}/setup/confirm-participants`);
+    await click(testSelector('link', 'add-participants'));
+    expect(currentURL()).to.equal(`/raffles/${raffle.id}/setup/add-participants`);
+  });
+
+  it('goes to the setup info route when clicking the save button', async function() {
+    await visit(`/raffles/${raffle.id}/setup/confirm-participants`);
+    await click(testSelector('link', 'confirm-participants'));
+    expect(currentURL()).to.equal(`/raffles/${raffle.id}/setup`);
+  });
+});
--- tests/integration/components/raffle-setup-confirm-participants-test.js
+++ tests/integration/components/raffle-setup-confirm-participants-test.js
@@ -1,24 +1,59 @@
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import { describe, it, beforeEach } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import { find } from 'ember-native-dom-helpers';
+import { makeList, manualSetup } from 'ember-data-factory-guy';
+import testSelector from 'ember-test-selectors';
 
 describe('Integration | Component | raffle setup confirm participants', function() {
   setupComponentTest('raffle-setup-confirm-participants', {
     integration: true
   });
 
-  it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#raffle-setup-confirm-participants}}
-    //     template content
-    //   {{/raffle-setup-confirm-participants}}
-    // `);
+  beforeEach(function() {
+    manualSetup(this.container);
+  });
 
-    this.render(hbs`{{raffle-setup-confirm-participants}}`);
-    expect(this.$()).to.have.length(1);
+  function render() {
+    this.render(hbs`
+      {{raffle-setup-confirm-participants participants=participants}}
+    `);
+  }
+
+  describe('content', function() {
+    beforeEach(function() {
+      render.call(this);
+    });
+    it('has a header', function() {
+      expect(find(testSelector('confirm-participants-header')).textContent.trim()).to.equal("Is this everyone?");
+    });
+
+    it('has a table', function() {
+      expect(find('table')).to.be.ok;
+    });
+  });
+
+  describe('table', function() {
+    beforeEach(function() {
+      this.set('participants', makeList('participant', 2));
+      render.call(this);
+    });
+
+    it('show the participants names', function() {
+      let participants = this.get('participants');
+      expect(find('.lt-body').textContent).to.include(participants.get('firstObject.name'));
+      expect(find('.lt-body').textContent).to.include(participants.get('lastObject.name'));
+    });
+  });
+
+  it('has an edit button', function() {
+    render.call(this);
+    expect(find(testSelector('link', 'confirm-participants'))).to.be.ok;
+  });
+
+  it('has a confirm link', function() {
+    render.call(this);
+    expect(find(testSelector('link', 'confirm-participants'))).to.be.ok;
   });
 });
