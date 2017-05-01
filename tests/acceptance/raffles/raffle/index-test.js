41ed0531e3ee71d6661808e56cf09e16a3369e57
Ben Limmer <ben@benlimmer.com>
2017-04-30 18:52:52 -0600

    fix tests.

--- tests/acceptance/raffles/raffle/index-test.js
+++ tests/acceptance/raffles/raffle/index-test.js
@@ -1,8 +1,8 @@
-import { describe, it, beforeEach, afterEach, context } from 'mocha';
+import { describe, it, beforeEach, afterEach } from 'mocha';
 import { expect } from 'chai';
 import startApp from 'raffle/tests/helpers/start-app';
 import destroyApp from 'raffle/tests/helpers/destroy-app';
-import { visit, click, find } from 'ember-native-dom-helpers';
+import { visit, click } from 'ember-native-dom-helpers';
 import { make } from 'ember-data-factory-guy';
 import testSelector from 'ember-test-selectors';
 
