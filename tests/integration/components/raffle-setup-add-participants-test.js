9fc5470d6e57a64ebedf52a43ecdabfa9f4919bb
Ben Limmer <ben@benlimmer.com>
2017-04-30 19:33:13 -0600

    add-participants test.

--- tests/integration/components/raffle-setup-add-participants-test.js
+++ tests/integration/components/raffle-setup-add-participants-test.js
@@ -1,24 +1,112 @@
+import Ember from 'ember';
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import { describe, it, beforeEach } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import { find, fillIn, click } from 'ember-native-dom-helpers';
+import { make, makeList, manualSetup } from 'ember-data-factory-guy';
+import testSelector from 'ember-test-selectors';
 
 describe('Integration | Component | raffle setup add participants', function() {
   setupComponentTest('raffle-setup-add-participants', {
     integration: true
   });
 
-  it.skip('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#raffle-setup-add-participants}}
-    //     template content
-    //   {{/raffle-setup-add-participants}}
-    // `);
+  beforeEach(function() {
+    manualSetup(this.container);
+  });
 
-    this.render(hbs`{{raffle-setup-add-participants}}`);
-    expect(this.$()).to.have.length(1);
+  function render() {
+    if (!this.get('raffle')) {
+      this.set('raffle', make('raffle'));
+    }
+
+    if (!this.get('confirmParticipants')) {
+      this.set('confirmParticipants', () => { });
+    }
+
+    this.render(hbs`
+      {{raffle-setup-add-participants
+        raffle=raffle
+        confirmParticipants=confirmParticipants}}
+    `);
+  }
+
+  describe('content', function() {
+    beforeEach(function() {
+      render.call(this);
+    });
+    it('has a header', function() {
+      expect(find(testSelector('add-participants-header')).textContent.trim()).to.equal("Who's participating in this raffle?");
+    });
+
+    it('has a textarea', function() {
+      expect(find('textarea')).to.be.ok;
+    });
+
+    it('has a button to save changes', function() {
+      expect(find(testSelector('add-participants-button'))).to.be.ok;
+    });
+  });
+
+  describe('textarea', function() {
+    it('prefills with existing participants', function() {
+      let participants = makeList('participant', 2);
+      let raffle = make('raffle', {
+        participants
+      });
+      this.set('raffle', raffle);
+      render.call(this);
+      expect(find('textarea').value).to.include(participants[0].get('name'));
+      expect(find('textarea').value).to.include(participants[1].get('name'));
+    });
+  });
+
+  describe('add participants button', function() {
+    let store;
+    beforeEach(function() {
+      store = Ember.getOwner(this).lookup('service:store');
+    });
+
+    it('creates paricipants that do not yet exist', async function() {
+      render.call(this);
+      expect(store.peekAll('participant').get('length')).to.equal(0);
+      await fillIn('textarea', 'New Guy 1\nNew Guy 2');
+      await click(testSelector('add-participants-button'));
+      let participants = store.peekAll('participant');
+      expect(participants.get('length')).to.equal(2);
+      expect(participants.get('firstObject.name')).to.equal('New Guy 1');
+      expect(participants.get('lastObject.name')).to.equal('New Guy 2');
+    });
+
+    // Not sure what's wrong with this test, but the store FactoryGuy is using
+    // is different than the one injected into the component, it seems.
+    it.skip('does not create participants that already exist', async function() {
+      render.call(this);
+      make('participant', {
+        name: 'New Guy 1'
+      });
+      expect(store.peekAll('participant').get('length')).to.equal(1);
+      await fillIn('textarea', 'New Guy 1\nNew Guy 2');
+      await click(testSelector('add-participants-button'));
+      let participants = store.peekAll('participant');
+      expect(participants.get('length')).to.equal(2);
+    });
+
+    it('associates participants with the raffle', async function() {
+      render.call(this);
+      await fillIn('textarea', 'New Guy 1\nNew Guy 2');
+      await click(testSelector('add-participants-button'));
+      expect(this.get('raffle.participants.length')).to.equal(2);
+    });
+
+    it('calls the confirmParticipants action', async function(done) {
+      this.set('confirmParticipants', () => {
+        done();
+      });
+      render.call(this);
+      await fillIn('textarea', 'New Guy 1\nNew Guy 2');
+      await click(testSelector('add-participants-button'));
+    });
   });
 });
