1eee836414ea7f5b9d8d8a96d9277dfffe21b4f6
Ben Limmer <ben@benlimmer.com>
2017-05-07 14:42:54 -0600

    Add simple tests for drawing grid.

--- app/components/drawing-grid.js
+++ app/components/drawing-grid.js
@@ -3,17 +3,12 @@ import { task, timeout } from 'ember-concurrency';
 import { round, sampleSize, without } from 'lodash';
 
 export default Ember.Component.extend({
-  winners: null,
-  losers: null,
   init() {
     this._super(...arguments);
 
-    let raffle = this.get('raffle');
-    this.setProperties({
-      winners: raffle.get('winners'),
-      losers: raffle.get('losers'),
-    });
-    this.get('dropLosers').perform();
+    if (!Ember.testing) {
+      this.get('dropLosers').perform();
+    }
   },
 
   currentParticipants: Ember.computed.union('winners', 'losers'),
@@ -23,8 +18,6 @@ export default Ember.Component.extend({
     let numToDrop = round(this.get('losers.length') * .2 , 0) || 1;
     let losersToDrop = sampleSize(losers, numToDrop);
     this.set('losers', without(losers, ...losersToDrop));
-
-    if (Ember.testing) { return; }
 
     yield timeout(1000);
 
--- tests/integration/components/drawing-grid-test.js
+++ tests/integration/components/drawing-grid-test.js
@@ -1,24 +1,46 @@
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import { describe, it, beforeEach } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import { find } from 'ember-native-dom-helpers';
+import { make, makeList, manualSetup } from 'ember-data-factory-guy';
+import testSelector from 'ember-test-selectors';
 
 describe('Integration | Component | drawing grid', function() {
   setupComponentTest('drawing-grid', {
     integration: true
   });
 
-  it.skip('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#drawing-grid}}
-    //     template content
-    //   {{/drawing-grid}}
-    // `);
-
-    this.render(hbs`{{drawing-grid}}`);
-    expect(this.$()).to.have.length(1);
+  beforeEach(function() {
+    manualSetup(this.container);
   });
+
+  function render() {
+    if (!this.get('winners')) {
+      this.set('winners', [make('participant')]);
+    }
+
+    if (!this.get('losers')) {
+      this.set('losers', makeList('participant', 2));
+    }
+
+    this.render(hbs`
+      {{drawing-grid winners=winners losers=losers  }}
+    `);
+  }
+
+  it('shows all participants in a grid', function() {
+    let participants = makeList('participant', 3);
+    this.set('winners', participants.slice(0));
+    this.set('losers', participants.slice(1,2));
+
+    render.call(this);
+
+    participants.forEach((participant) => {
+      expect(find(testSelector('participant-id', participant.get('id')))).to.be.ok;
+    });
+  });
+
+  it.skip('pulls losers from the board');
+  it.skip('calls the showWinners action when all losers have been pulled from the board');
 });
