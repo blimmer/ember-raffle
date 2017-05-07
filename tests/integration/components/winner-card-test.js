9467906ba1e54f0d51e7be8be67336f4dff65d29
Ben Limmer <ben@benlimmer.com>
2017-05-07 14:32:01 -0600

    Test winner card.

--- tests/integration/components/winner-card-test.js
+++ tests/integration/components/winner-card-test.js
@@ -2,23 +2,24 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import { find } from 'ember-native-dom-helpers';
+import { make, manualSetup } from 'ember-data-factory-guy';
+import testSelector from 'ember-test-selectors';
 
 describe('Integration | Component | winner card', function() {
   setupComponentTest('winner-card', {
     integration: true
   });
 
-  it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#winner-card}}
-    //     template content
-    //   {{/winner-card}}
-    // `);
-
+  it('shows a trophy icon', function() {
     this.render(hbs`{{winner-card}}`);
-    expect(this.$()).to.have.length(1);
+    expect(find('i.icon.trophy')).to.be.ok;
+  });
+
+  it("includes the winner's name", function() {
+    manualSetup(this.container);
+    this.set('winner', make('participant', { name: 'Lucky Ducky' }));
+    this.render(hbs`{{winner-card winner=winner}}`);
+    expect(find(testSelector('winner-name')).textContent.trim()).to.equal('Lucky Ducky');
   });
 });
