d32adf84da2354265eeeac5471259a9c46667670
Ben Limmer <ben@benlimmer.com>
2017-07-15 14:27:33 -0500

    Shuffle initial participants.

--- app/components/drawing-grid.js
+++ app/components/drawing-grid.js
@@ -1,27 +1,38 @@
 import Ember from 'ember';
 import { task, timeout } from 'ember-concurrency';
-import { round, sampleSize, without } from 'lodash';
+import { round, sampleSize, shuffle, without } from 'lodash';
 
 export default Ember.Component.extend({
-  init() {
+  didReceiveAttrs() {
     this._super(...arguments);
+
+    this.set('losersRemainingToDrop', this.get('losers'));
 
     if (!Ember.testing) {
       this.get('dropLosers').perform();
     }
   },
 
-  currentParticipants: Ember.computed.union('winners', 'losers'),
+  losersRemainingToDrop: null, // set as clone on didReceiveAttrs
+  allParticipants: Ember.computed.union('winners', 'losers'),
+  currentParticipants: Ember.computed('allParticipants.[]', function() {
+    return shuffle(this.get('allParticipants'));
+  }),
 
   dropLosers: task(function * () {
-    let losers = this.get('losers').toArray();
-    let numToDrop = round(this.get('losers.length') * .2 , 0) || 1;
-    let losersToDrop = sampleSize(losers, numToDrop);
-    this.set('losers', without(losers, ...losersToDrop));
+    let currentParticipants = this.get('currentParticipants');
+    let losersRemainingToDrop = this.get('losersRemainingToDrop').toArray();
+    let numToDrop = round(losersRemainingToDrop.get('length') * .2 , 0) || 1;
+    let losersToDropNow = sampleSize(losersRemainingToDrop, numToDrop);
+
+    this.setProperties({
+      losersRemainingToDrop: without(losersRemainingToDrop, ...losersToDropNow),
+      currentParticipants: without(currentParticipants, ...losersToDropNow),
+    });
 
     yield timeout(1000);
 
-    if (this.get('losers.length') > 0) {
+    if (this.get('losersRemainingToDrop.length') > 0) {
       this.get('dropLosers').perform();
     } else {
       this.sendAction('showWinners');
