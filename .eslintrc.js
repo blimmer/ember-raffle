c2328e07616fcc5752c959a64284b88f501d82ae
Ben Limmer <ben@benlimmer.com>
2020-04-03 12:01:30 -0600

    Reinit to get eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,8 @@
-'use strict';
-
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
+    ecmaVersion: 2017,
+    sourceType: 'module'
   },
   plugins: [
     'ember'
@@ -21,36 +15,26 @@ module.exports = {
     browser: true
   },
   rules: {
-    'ember/no-jquery': 'error'
   },
   overrides: [
     // node files
     {
       files: [
-        '.eslintrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js',
-        'server/**/*.js'
+        'lib/*/index.js'
       ],
       parserOptions: {
-        sourceType: 'script'
+        sourceType: 'script',
+        ecmaVersion: 2015
       },
       env: {
         browser: false,
         node: true
-      },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      })
+      }
     }
   ]
 };
