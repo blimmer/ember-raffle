7576be5d6252cbccb187cd7ee372f1af986c1f22
Ben Limmer <ben@benlimmer.com>
2020-04-03 16:22:15 -0600

    Fix app controller deprecation

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,11 @@
+import { inject as service } from "@ember/service";
 import { dasherize } from "@ember/string";
 import { computed } from "@ember/object";
 import Controller from "@ember/controller";
 
 export default Controller.extend({
-  routeCssName: computed("currentRouteName", function () {
-    return dasherize(this.get("currentRouteName"));
+  router: service(),
+  routeCssName: computed("router.currentRouteName", function () {
+    return dasherize(this.get("router.currentRouteName"));
   }),
 });
--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,7 +3,7 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchId: "application-controller.router-properties" },
+    { handler: "throw", matchId: "application-controller.router-properties" },
     { handler: "silence", matchId: "ember-intl.translationMacro" },
     { handler: "silence", matchId: "computed-property.override" },
     { handler: "silence", matchId: "ember-component.send-action" },
