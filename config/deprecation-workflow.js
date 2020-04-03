e99db6e9ba1b99b05ba8ffe1f98d5e52a2bd82e8
Ben Limmer <ben@benlimmer.com>
2020-04-03 16:03:09 -0600

    Fix warnings about bare strings

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,9 @@
-/* eslint-env browser */
-
-window.deprecationWorkflow = window.deprecationWorkflow || {};
-window.deprecationWorkflow.config = {
-  workflow: []
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "silence", matchId: "application-controller.router-properties" },
+    { handler: "silence", matchId: "ember-intl.translationMacro" },
+    { handler: "silence", matchId: "computed-property.override" },
+    { handler: "silence", matchId: "ember-component.send-action" },
+  ],
 };
